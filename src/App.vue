9b148c37642b03ccee0d0465d7a14b202b9ef5a7
takashi <b7180@oic.jp>
2018-01-05 02:19:29 +0900

    画面幅 文字色修正
    fixed #5 #6

--- src/App.vue
+++ src/App.vue
@@ -22,17 +22,19 @@ export default {
   }
 
   .box {
-    border: solid 5px #000;
-    margin: auto;
-    width: auto;
-    height: 400px;
-    overflow: hidden;
     .comment {
-      font-size: 120%;
+      font-size: 150%;
+      color: white;
+      text-shadow:
+          black 0.5px 0.5px 0, black -0.5px -0.5px 0,/*右下、左上*/
+          black -0.5px 0.5px 0, black 0.5px -0.5px 0,/*右上、左下*/
+          black 0px 0.5px 0, black  -0.5px 0,/*右、左*/
+          black -0.5px 0 0, black 0.5px 0 0;/*上、下*/
       animation-name: comments;
       animation-duration: 10s;
       animation-timing-function: linear;
       animation-iteration-count: 1;
+      animation-fill-mode: both;
     }
 
     @keyframes comments {
