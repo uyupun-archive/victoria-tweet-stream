62833ea22ae2459eee117431575aefebee55bc3f
Kazuki Tobita <kazukiti201@gmail.com>
2018-01-21 14:54:34 +0900

    アニメーションがループするようにした

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
   #app
     .box
-      .comment ほげほげほげほげほげほげ
-      .comment ほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげ
+      .comment#comment1 ほげほげほげほげほげほげ
+      .comment#comment2 ほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげ
 </template>
 
 <script>
@@ -23,30 +23,30 @@ export default {
 
   .box {
     position: relative;
-      .comment {
-        position: absolute;
-        right: 0;
-        font-size: 32px;
-        white-space: nowrap;
-        color: white;
-        text-shadow:
-          black 0.7px 0.7px 0, black -0.7px -0.7px 0,/*右下、左上*/
-          black -0.7px 0.7px 0, black 0.7px -0.7px 0,/*右上、左下*/
-          black 0 0.7px 0, black  -0.7px 0,/*右、左*/
-          black -0.7px 0 0, black 0.7px 0 0;/*上、下*/
-        animation-name: comments;
-        animation-duration: 25s;
-        animation-iteration-count: 1;
-        animation-fill-mode: both;
-        animation-timing-function: linear;
+    .comment {
+      position: absolute;
+      right: 0;
+      font-size: 32px;
+      white-space: nowrap;
+      color: white;
+      text-shadow:
+        black 0.7px 0.7px 0, black -0.7px -0.7px 0,/*右下、左上*/
+        black -0.7px 0.7px 0, black 0.7px -0.7px 0,/*右上、左下*/
+        black 0 0.7px 0, black  -0.7px 0,/*右、左*/
+        black -0.7px 0 0, black 0.7px 0 0;/*上、下*/
+      animation-name: comments;
+      animation-duration: 25s;
+      animation-iteration-count: infinite;
+      animation-fill-mode: both;
+      animation-timing-function: linear;
+    }
+    @keyframes comments {
+      from {
+        transform: translate(101%, 0);
       }
-      @keyframes comments {
-        from {
-          transform: translate(101%, 0);
-        }
-        to {
-          transform: translate(-4500px, 0);
-        }
+      to {
+        transform: translate(-4500px, 0);
       }
+    }
   }
 </style>
